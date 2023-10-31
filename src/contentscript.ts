85166601bb6799cf3efc3638b369b9e4e3d7a39b
kubitus2 <jakubosinski2@gmail.com>
2023-10-31 18:14:04 +0100

    Refactor.

--- src/contentscript.ts
+++ src/contentscript.ts
@@ -1,9 +1,3 @@
-import { getButton, appendElements, getSpan } from './htmlUtils'
-import {
-  incrementRate,
-  decrementRate,
-  obtainRateFromVideo,
-} from './playbackRate'
 import {
   YT_CONTROL_DIV_CLASS,
   LEFT_BTN,
@@ -11,79 +5,123 @@ import {
   LABEL,
   RESET_BTN,
 } from './consts'
+import {
+  incrementRate,
+  decrementRate,
+  obtainRateFromVideo,
+} from './playbackRate'
+import { getButton, appendElements, getSpan } from './htmlUtils'
 
-let controls: HTMLDivElement | null
-let label: HTMLSpanElement | null
-let rate = 1
+class PlaybackController {
+  private controls: HTMLDivElement | null = null
+  private label: HTMLSpanElement | null = null
+  private addedElements: HTMLElement[] = []
+  private rate = 1
 
-let addedElements: HTMLElement[] = []
-
-window.addEventListener('yt-navigate-finish', () => {
-  console.log('New page loaded')
-
-  if (fetchElements() < 0) return
-
-  setupControlElements()
-})
-
-const fetchElements = (): number => {
-  controls = document.querySelector(YT_CONTROL_DIV_CLASS)
-  if (!controls) {
-    console.error('Control elements not found!')
-    return -1
+  public start() {
+    window.addEventListener('yt-navigate-finish', this.onPageLoad.bind(this))
   }
 
-  return 0
-}
+  /*private onRPressed = (e: KeyboardEvent) => {
+    if (e.key === 'r' && e.key === 'r') {
+      console.log('r')
+      this.clickReset()
+    }
+  }*/
 
-const setupControlElements = () => {
-  rate = obtainRateFromVideo()
+  private setupControlElements(): void {
+    this.clearControlElements()
+    this.rate = obtainRateFromVideo()
 
-  const btnLeft = getButton(LEFT_BTN, '<b><</b>', clickDecrement)
-  const btnRight = getButton(RIGHT_BTN, '<b>></b>', clickIncrement)
-  const resetBtn = getButton(RESET_BTN, '<b>RESET</b>', clickReset)
+    const btnLeft = getButton(
+      LEFT_BTN,
+      '<b><</b>',
+      this.clickDecrement.bind(this)
+    )
+    const btnRight = getButton(
+      RIGHT_BTN,
+      '<b>></b>',
+      this.clickIncrement.bind(this)
+    )
+    const resetBtn = getButton(
+      RESET_BTN,
+      '<b>RESET</b>',
+      this.clickReset.bind(this)
+    )
 
-  const spanResponse = getSpan(LABEL, rate.toString())
+    //document.addEventListener('keyup', this.onRPressed.bind(this))
 
-  label = spanResponse.element
-  if (!spanResponse.present) {
-    console.log(spanResponse.element)
-    addedElements = [btnLeft, spanResponse.element, btnRight, resetBtn]
-    appendElements(addedElements, controls!)
+    const spanResponse = getSpan(LABEL, this.rate.toString())
+
+    this.label = spanResponse.element
+    if (!spanResponse.present) {
+      this.addedElements = [btnLeft, spanResponse.element, btnRight, resetBtn]
+      appendElements(this.addedElements, this.controls!)
+    }
+  }
+
+  private fetchElements(): number {
+    this.controls = document.querySelector(YT_CONTROL_DIV_CLASS)
+    if (!this.controls) {
+      console.error('Control elements not found!')
+      return -1
+    }
+
+    return 0
+  }
+
+  private clickDecrement() {
+    const newRate = decrementRate(this.rate)
+    this.rate = newRate
+    this.updateRate(newRate)
+  }
+
+  private clickIncrement() {
+    const newRate = incrementRate(this.rate)
+    this.rate = newRate
+
+    this.updateRate(newRate)
+  }
+
+  private clickReset() {
+    this.rate = 1
+    this.updateRate(1)
+  }
+
+  private updateRate(newRate: number) {
+    const video = document.querySelector('video')
+
+    if (!video) {
+      console.error('No video element found')
+      return
+    }
+
+    video.playbackRate = newRate
+    if (!this.label) {
+      console.error('Label not found')
+      return
+    }
+
+    this.label.innerHTML = newRate.toString()
+  }
+
+  private onPageLoad(): void {
+    console.log('New page loaded')
+
+    if (this.fetchElements() >= 0) {
+      this.setupControlElements()
+    }
+  }
+
+  private clearControlElements(): void {
+    this.addedElements.forEach(element => {
+      if (element.parentNode) {
+        element.parentNode.removeChild(element)
+      }
+    })
+    this.addedElements = []
   }
 }
 
-const clickDecrement = () => {
-  const newRate = decrementRate(rate)
-  rate = newRate
-  updateRate(newRate)
-}
-
-const clickIncrement = () => {
-  const newRate = incrementRate(rate)
-  rate = newRate
-
-  updateRate(newRate)
-}
-
-const clickReset = () => {
-  rate = 1
-  updateRate(1)
-}
-
-const updateRate = (newRate: number) => {
-  const video = document.querySelector('video')
-
-  if (!video) {
-    console.error('No video element found')
-    return
-  }
-
-  video.playbackRate = newRate
-  if (!label) {
-    console.error('Label not found')
-    return
-  }
-
-  label.innerHTML = newRate.toString()
-}
+const controller = new PlaybackController()
+controller.start()
